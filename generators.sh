a979ff33140a0d1a8f7072384be7f04ecdbe09dd
asrcpq <asrcpq@gmail.com>
2020-02-04 02:17:56 +0000

    add flattendir

--- generators.sh
+++ generators.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-filelist=$(ls -r src | egrep '^[0-9]{6}')
+flattendir = "flatten"
+filelist="$(ls -r "$flattendir" | egrep '^[0-9]{6}')"
 ls output | egrep '^[0-9]{6}' | xargs -i rm output/{}
 echo '<html><head>
 <link href="style.css" rel="stylesheet" type="text/css" />
 <meta http-equiv="content-type" content="text/html;charset=utf-8">
 </head><body id="navibody"><div id="navidiv">' > output/navi.html
 lastmonth="0000"
-for file in $filelist; do
-	filehead=$(echo $file | egrep -o '^[^.]*')
-	title=$(cat src/$file | head -1 | sed 's/^# //g')
+for file in "$filelist"; do
+	filehead=$(echo "$file" | egrep -o '^[^.]*')
+	title=$(cat "$flattendir"/"$file" | head -1 | sed 's/^# //g')
 	month=$(echo $file | grep -o '^....') 
 	if [ "$month" != "$lastmonth" ]; then
 		echo '<hr>' >> output/navi.html
@@ -17,7 +18,7 @@ for file in $filelist; do
 	lastmonth=$month
 	echo "<a draggable="false" href=\"$filehead.html\"" \
 			"target=bodyinfo>$title<br></a>" >> output/navi.html
-	pandoc src/$file --css pandoc.css -o output/$filehead.html -s
+	pandoc "$flattendir"/"$file" --css pandoc.css -o output/$filehead.html -s
 done
 
 echo '<hr></div></body></html>' >> output/navi.html
