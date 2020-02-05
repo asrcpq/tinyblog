f447bf6810da014e5460eecaa16964a7b7aaa909
asrcpq <asrcpq@gmail.com>
2020-02-05 04:53:59 +0000

    no flatten now

--- build.sh
+++ build.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-flattendir="flatten"
+set -e
 scriptdir="$(dirname $0)"
 cd $scriptdir
-rm -rf "$flattendir"
-mkdir "$flattendir"
-cd "$flattendir"
-find ../src/ -type f -name "*.md" | egrep '.*/[0-9]{6}[^/]*\.md' | xargs -i ln -s {} ./
-cd ..
-
-filelist="$(ls -r "$flattendir" | egrep '^[0-9]{6}')"
+filelist=$(find ./src -type f -name "*.md" | egrep '.*/[0-9]{6}[^/]*\.md')
+if [ ! -d output ]; then
+	mkdir output
+fi
 ls output | egrep '^[0-9]{6}' | xargs -i rm output/{}
 echo '<html><head>
 <link href="style.css" rel="stylesheet" type="text/css" />
 <meta http-equiv="content-type" content="text/html;charset=utf-8">
 </head><body id="navibody"><div id="navidiv">' > output/navi.html
 lastmonth="0000"
-for file in "$filelist"; do
-	filehead=$(echo "$file" | egrep -o '^[^.]*')
-	title=$(cat "$flattendir"/"$file" | head -1 | sed 's/^# //g')
-	month=$(echo $file | grep -o '^....') 
+echo "$filelist" | while read -r each_fullname; do
+	echo "$each_fullname"
+	filename=$(echo "$each_fullname" | sed -E 's/.*\/()/\1/g')
+	filehead=$(echo "$filename" | egrep -o '^[^.]*')
+	title=$(cat "$each_fullname" | head -1 | sed 's/^# //g')
+	month=$(echo $filename | grep -o '^....') 
 	if [ "$month" != "$lastmonth" ]; then
 		echo '<hr>' >> output/navi.html
 		echo "<h3>$month</h3>" >> output/navi.html
@@ -26,7 +25,7 @@ for file in "$filelist"; do
 	lastmonth=$month
 	echo "<a draggable="false" href=\"$filehead.html\"" \
 			"target=bodyinfo>$title<br></a>" >> output/navi.html
-	pandoc "$flattendir"/"$file" --css pandoc.css -o output/$filehead.html -s
+	pandoc "$each_fullname" --metadata pagetitle="$title" --css pandoc.css -o output/$filehead.html -s
 done
 
 echo '<hr></div></body></html>' >> output/navi.html
